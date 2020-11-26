841144d928ffff1b7e55cb4a15e40665703ed5b2
natka83 <fastnat.nf@gmail.com>
2020-11-26 07:34:51 +0200

    new commit

--- src/app/my-hobby/my-hobby.component.ts
+++ src/app/my-hobby/my-hobby.component.ts
@@ -7,13 +7,19 @@ import { Hobby } from './hobby';
   styleUrls: ['./my-hobby.component.css']
 })
 export class MyHobbyComponent implements OnInit {
-  myHobby: Hobby;
+  myHobby1: Hobby;
+  myHobby2: Hobby;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.myHobby = new Hobby();
-    this.myHobby.title ="My hobby is reading.";
-    this.myHobby.discription ="It has been a great pleasure for me since my childhood. The world of literature is fascinating and infinite."
+    this.myHobby1 = new Hobby();
+    this.myHobby1.title ="My hobby is reading.";
+    this.myHobby1.discription ="It has been a great pleasure for me since my childhood. The world of literature is fascinating and infinite."
+
+    this.myHobby2 = new Hobby();
+    this.myHobby2.title ="My hobby is driving.";
+    this.myHobby2.discription ="I love driving on our country. It's so cool to discover a cities, a road with a beautiful view, or a place that seems like a secret one."
   }
 
 }
